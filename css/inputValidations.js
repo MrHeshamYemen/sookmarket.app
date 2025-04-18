0d8dbc64b0ca8d8bf6118bc597070ca3f82e40b7
Mr.Hesham.Yemen <88216423+MrHeshamYemen@users.noreply.github.com>
2025-04-18 21:02:27 +0300

    Add files via upload

---
+++ css/inputValidations.js
@@ -0,0 +1,31 @@
+﻿const reSpaces = /^\S*$/;
+
+function validatePassword() {
+    let e = document.getElementById("password");
+    return e.classList.remove("is-valid"), e.classList.remove("is-invalid"), !(e.value.length < 6) || (e.classList.add("is-invalid"), !1)
+}
+
+function validateEmail() {
+    let e = document.getElementById("email"),
+        i = document.getElementById("emailValidationText");
+    return (e.classList.remove("is-valid"), e.classList.remove("is-invalid"), 0 == e.value.length) ? (i.innerText = "Enter the user id", e.classList.add("is-invalid"), !1) : !!(reSpaces.test(e.value) && /^([a-zA-Z0-9_\-?\.?]){3,}@([a-zA-Z0-9]){3,}\.([a-zA-Z]){2,5}$/.test(e.value)) || (e.classList.add("is-invalid"), i.innerText = "User Id incorrect", !1)
+}
+
+function scrollToFirstInvlidElement() {
+    var e = document.querySelector(".is-invalid");
+    e && e.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+        inline: "nearest",
+        block: "center",
+        inline: "center",
+        offsetTop: -100
+    })
+}
+
+function removeBootStrapValidationClassess(e) {
+    e && (e.classList.remove("is-valid"), e.classList.remove("is-invalid"))
+}
+
+ 
+ 
